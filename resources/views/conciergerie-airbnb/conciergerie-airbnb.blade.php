32b8d27ee16c058deeafc73645616cb895c0d141
Cdric-art <cdrc-art@gmail.com>
2024-04-03 23:01:13 +0200

    starting inte conciergerie airbnb

--- resources/views/conciergerie-airbnb/conciergerie-airbnb.blade.php
+++ resources/views/conciergerie-airbnb/conciergerie-airbnb.blade.php
@@ -3,6 +3,63 @@
 
         <x-title title="LOCATION AIRBNB" subtitle="Comment ça marche ?" color="wht"/>
 
+        <section class="max-w-8xl flex flex-wrap items-center mx-auto">
+
+            @foreach($posts as $post)
+            <div class="flex flex-wrap justify-center items-center my-16 mx-auto">
+
+                @if(empty($post->title) && empty($post->contentInverse) && !empty($post->content))
+                <p class="md:text-1xl text-center text-wht p-4 w-full md:mx-40 xl:mx-60">{{ $post->content }}</p>
+                @endif
+
+                @if(!empty($post->title))
+
+                @if(!$post->inverseContent)
+
+                <div class="relative">
+                    <img class="max-h-[300px] max-w-[380px] sm:max-w-[455px]"
+                         src="{{ asset('images/' . $post->image) }}" alt="image">
+                    @if(empty($post->content))
+                    <h3 class="block absolute bottom-0 left-0 right-0 text-3xl text-pinkLight pt-2">{{ $post->title
+                        }}</h3>
+                    @endif
+                </div>
+                @if(!empty($post->content))
+                <div
+                    class="flex flex-col justify-center items-start min-h-[300px] sm:w-[600px] text-wht bg-pinkDark px-8 py-4">
+                    <h3 class="text-3xl md:text-6xl pb-2">{{ $post->title }}</h3>
+                    <p class="text-start md:text-1xl">{{ $post->content }}</p>
+                </div>
+                @endif
+
+                @endif
+
+                @if($post->inverseContent)
+
+                @if(!empty($post->content))
+                <div
+                    class="flex flex-col justify-center items-start min-h-[300px] sm:w-[600px] text-wht bg-pinkDark px-8 py-4">
+                    <h3 class="text-start text-3xl md:text-6xl pb-2">{{ $post->title }}</h3>
+                    <p class="text-start md:text-1xl">{{ $post->content }}</p>
+                </div>
+                @endif
+                <div>
+                    <img class="max-h-[300px] max-w-[380px] sm:max-w-[455px]"
+                         src="{{ asset('images/' . $post->image) }}" alt="image">
+                    @if(empty($post->content))
+                    <h3 class="text-4xl text-pinkLight pt-2">{{ $post->title }}</h3>
+                    @endif
+                </div>
+
+                @endif
+
+                @endif
+
+            </div>
+
+            @endforeach
+
+        </section>
 
     </section>
 </x-home-layout>
